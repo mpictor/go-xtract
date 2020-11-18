a85f81080d7f639b19424bdeda3b0817e5757b52
Mark Pictor <mpictor@gmail.com>
2020-11-18 17:55:56 -0600

    integ tests: make a go generate step possible
    Signed-off-by: Mark Pictor <mpictor@gmail.com>

--- _integration/main_test.go
+++ _integration/main_test.go
@@ -3,21 +3,23 @@ package main
 import (
 	"bytes"
 	"fmt"
-	"github.com/stretchr/testify/assert"
-	"github.com/stretchr/testify/require"
-	"gopkg.in/yaml.v2"
 	"io/ioutil"
 	"os/exec"
 	"path/filepath"
 	"sort"
 	"strings"
 	"testing"
+
+	"github.com/stretchr/testify/assert"
+	"github.com/stretchr/testify/require"
+	"gopkg.in/yaml.v2"
 )
 
 type spec struct {
 	Command    string `yaml:"cmd"`
 	Output     string `yaml:"output"`
 	ShouldFail bool   `yaml:"should_fail"`
+	Generate   bool   `yaml:"generate"`
 }
 
 func TestE2E(t *testing.T) {
@@ -72,19 +74,29 @@ func (r *testRunner) loadSpec() {
 }
 
 func (r *testRunner) runCommand() string {
+	if r.testSpec.Generate {
+		src := "./" + r.testName + "/src/..."
+		gen := exec.Command("go", "generate", src)
+		out, err := gen.CombinedOutput()
+		require.NoError(r.t, err, "%v exited with %s\nfull output:\n%s", gen.Args, err, string(out))
+		if err == nil {
+			r.t.Logf("%v: success", gen.Args)
+		}
+	}
 	args := strings.Split(r.testSpec.Command, " ")
 	first, rest := args[0], args[1:]
 
 	cmd := exec.Command(first, rest...)
-	var output bytes.Buffer
+	var output, stderr bytes.Buffer
 	cmd.Stdout = &output
+	cmd.Stderr = &stderr
 	cmd.Dir = r.testName
 
 	err := cmd.Run()
 	if r.testSpec.ShouldFail {
-		require.Error(r.t, err, "command must fail: '%s %s'", first, strings.Join(rest, " "))
+		require.Error(r.t, err, "command must fail: '%s %s'\nout:\n%s\nerr:\n%s", first, strings.Join(rest, " "), output.String(), stderr.String())
 	} else {
-		require.NoError(r.t, err, "command failed: '%s %s'", first, strings.Join(rest, " "))
+		require.NoError(r.t, err, "command failed: '%s %s'\nout:\n%s\nerr:\n%s", first, strings.Join(rest, " "), output.String(), stderr.String())
 	}
 	return output.String()
 }
