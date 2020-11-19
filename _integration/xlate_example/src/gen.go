1f1e550c6b7afa9dc64235c94a6b7ecef061a20f
Mark Pictor <mpictor@gmail.com>
2020-11-19 17:04:06 -0600

    xlate integ update

--- _integration/xlate_example/src/gen.go
+++ _integration/xlate_example/src/gen.go
@@ -5,8 +5,8 @@ package main
 //go:generate -command bindata go run github.com/jteeuwen/go-bindata/go-bindata
 
 //extract strings, overwriting the primary language's file (here, en-us.json)
-//NOTE: cannot use relative path for files to scan
-//go:generate xtract -func github.com/mpictor/go-xtract/pkg/xlate.T -j -o data/en-us.json **/*.go
+//NOTE: path for files to scan cannot begin with `./`
+//go:generate xtract -j -o data/en-us.json **/*.go
 
 //check all json files in data against the primary language's file (again, en-us.json)
 //go:generate xtract -v -c data/en-us.json
